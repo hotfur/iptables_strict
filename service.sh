571faa95debdcf2fe625e97fa437ca49e5c6094c
Vuong Kha Sieu <75152429+hotfur@users.noreply.github.com>
2024-07-17 23:41:36 +0700

    Add support for IPv6
    Sometimes the connected network have IPv6 capabilities. In these network, it is necessary to block the IPv6 traffics using ip6tables.

--- service.sh
+++ service.sh
@@ -2,3 +2,8 @@
 /system/bin/iptables -P FORWARD DROP
 /system/bin/iptables -A INPUT -i lo -j ACCEPT  # Allow localhost
 /system/bin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT  # Allow established connections
+
+/system/bin/ip6tables -P INPUT DROP
+/system/bin/ip6tables -P FORWARD DROP
+/system/bin/ip6tables -A INPUT -i lo -j ACCEPT  # Allow localhost
+/system/bin/ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT  # Allow established connections
